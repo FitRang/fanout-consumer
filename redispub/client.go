1cf49784d26d163b206c266b4f2e1531b56aed09
Foxtrot <noamanahmedai@gmail.com>
2026-02-23 22:17:38 +0530

    Fixed: startup issue

--- redispub/client.go
+++ redispub/client.go
@@ -3,7 +3,7 @@ package redispub
 import "github.com/redis/go-redis/v9"
 
 func NewRedisClient(conn string) (*redis.Client, error) {
-	opt, err := redis.ParseURL(conn)
+opt, err := redis.ParseURL(conn)
 	if err != nil {
 		return nil, err
 	}
